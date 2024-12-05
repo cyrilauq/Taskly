2930c66bf20e58ae9db6885737519f444f30ef4b
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-12-05 13:35:08 +0100

    extends base controller and authorize controller

--- api/src/TodoList.API/Controllers/TodoController.cs
+++ api/src/TodoList.API/Controllers/TodoController.cs
@@ -1,18 +1,18 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using TodoList.Application.Features.Todo.Queries.List;
 
 namespace TodoList.API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class TodoController(IMediator mediator) : ControllerBase
+    [Authorize]
+    public class TodoController(IMediator mediator) : BaseApiController(mediator)
     {
         [HttpGet]
-        public async Task<IActionResult> ListTodos()
+        public async Task<IActionResult> ListTodos([FromQuery] ListTodosQuery query)
         {
-            return Ok(await mediator.Send(new ListTodosQuery()));
+            return Ok(await Mediator.Send(query));
         }
     }
 }
