460a991f4729730b2bd9313d6c504b839c52ecd4
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-12-05 15:13:46 +0100

    add jwt authorization to swagger

--- api/src/TodoList.API/Program.cs
+++ api/src/TodoList.API/Program.cs
@@ -25,7 +25,7 @@ builder.Services.AddCorsConfiguration();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwagger();
 
 var app = builder.Build();
 
