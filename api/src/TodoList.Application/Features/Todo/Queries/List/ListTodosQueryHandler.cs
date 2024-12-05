dc37fcfde57607aa9ed2146f0b11e5682f9beb66
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-12-05 13:35:17 +0100

    authorize filering

--- api/src/TodoList.Application/Features/Todo/Queries/List/ListTodosQueryHandler.cs
+++ api/src/TodoList.Application/Features/Todo/Queries/List/ListTodosQueryHandler.cs
@@ -1,17 +1,30 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Http;
 using TodoList.Application.Args;
 using TodoList.Application.DTOs;
+using TodoList.Application.Extensions;
 using TodoList.Application.IRepository;
 
 namespace TodoList.Application.Features.Todo.Queries.List
 {
-    public class ListTodosQueryHandler(ICRUDRepository<Domain.Entities.Todo, TodoSearchArg> todoRepository) : IRequestHandler<ListTodosQuery, List<TodoDTO>>
+    public class ListTodosQueryHandler(ICRUDRepository<Domain.Entities.Todo, TodoSearchArg> todoRepository, IHttpContextAccessor httpContextAccessor) : IRequestHandler<ListTodosQuery, List<TodoDTO>>
     {
         public async Task<List<TodoDTO>> Handle(ListTodosQuery request, CancellationToken cancellationToken)
         {
-            return (await todoRepository.GetAllAsync(new TodoSearchArg { UserId = request.UserId, IsDeleted = request.IsDeleted }))
+            var connectedUserId = Guid.Parse(httpContextAccessor.ConnectedUserId()!);
+            if (!UserCanListTodo(connectedUserId, request.UserId, httpContextAccessor.ConnectedUserRoles())) throw new UnauthorizedAccessException("You can't see the request resource");
+            return (await todoRepository.GetAllAsync(new TodoSearchArg { UserId = connectedUserId, IsDeleted = request.IsDeleted }))
                 .Select(t => new TodoDTO(t.Id.ToString(), t.Content ?? "No content", t.Name, t.UserId, t.IsDone, t.CreatedOn, t.DeletedOn))
                 .ToList();
+        }
+
+        private bool UserCanListTodo(Guid connectedUserId, Guid requestUserId, IEnumerable<string> userRoles)
+        {
+            if(userRoles.Contains("Admin"))
+            {
+                return true;
+            }
+            return requestUserId == Guid.Empty || requestUserId == connectedUserId;
         }
     }
 }
