a6fd6bfaeb9d9084e9c7932ad3937e0a3b1027b1
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-12-05 13:34:57 +0100

    add base cotnroller

---
+++ api/src/TodoList.API/Controllers/BaseApiController.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TodoList.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseApiController(IMediator mediator) : ControllerBase
+    {
+        protected IMediator Mediator { get; } = mediator;
+    }
+}
