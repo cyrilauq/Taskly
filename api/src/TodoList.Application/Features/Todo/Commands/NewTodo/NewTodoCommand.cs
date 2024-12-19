ade0cf505eb2424b636ba158a989d55245d4e33f
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-12-19 20:32:49 +0100

    add a new command to create a new todo

---
+++ api/src/TodoList.Application/Features/Todo/Commands/NewTodo/NewTodoCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using TodoList.Application.DTOs;
+
+namespace TodoList.Application.Features.Todo.Commands.NewTodo
+{
+    public class NewTodoCommand : IRequest<TodoDTO>
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
---
+++ api/src/TodoList.Application/Features/Todo/Commands/NewTodo/NewTodoCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using MediatR;
+using TodoList.Application.DTOs;
+using TodoList.Application.IRepository;
+using TodoList.Application.IService;
+using UnauthorizedAccessException = TodoList.Application.Services.Exceptions.UnauthorizedAccessException;
+
+namespace TodoList.Application.Features.Todo.Commands.NewTodo
+{
+    public class NewTodoCommandHandler(IMapper mapper, ITodoRepository todoRepository, IUserContext userContext) : IRequestHandler<NewTodoCommand, TodoDTO>
+    {
+        public async Task<TodoDTO> Handle(NewTodoCommand request, CancellationToken cancellationToken)
+        {
+            if(!userContext.IsAuthenticated) throw new UnauthorizedAccessException("User need to be authenticated to add a new todo");
+            var todo = new Models.Todo
+            {
+                Name = request.Name,
+                Content = request.Description,
+                UserId = userContext.UserId
+            };
+            var addedTodo = await todoRepository.AddAsync(todo);
+            return mapper.Map<TodoDTO>(todo);
+        }
+    }
+}
