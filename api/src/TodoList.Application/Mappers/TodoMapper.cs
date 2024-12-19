0036103e278b7b679e620644dfa14bbbda140eb2
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-12-19 20:32:23 +0100

    add a mapper to map todo dto into entity and vice versa

---
+++ api/src/TodoList.Application/Mappers/TodoMapper.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using TodoList.Application.DTOs;
+using TodoList.Domain.Entities.Interfaces;
+
+namespace TodoList.Application.Mappers
+{
+    internal class TodoMapper: Profile
+    {
+        public TodoMapper()
+        {
+            CreateMap<TodoDTO, ITodo>()
+                .ReverseMap();
+        }
+    }
+}
