73e7a067cc2d5a889bd0c4f683dae70fbb6af803
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-12-19 20:30:19 +0100

    add a context accessor to access authenticated user's information outside of a controller

---
+++ api/src/TodoList.Application/IService/IUserContext.cs
@@ -0,0 +1,8 @@
+﻿namespace TodoList.Application.IService
+{
+    public interface IUserContext
+    {
+        bool IsAuthenticated { get; }
+        Guid UserId { get; }
+    }
+}
---
+++ api/src/TodoList.Application/Services/UserContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using TodoList.Application.Extensions;
+using TodoList.Application.IService;
+
+namespace TodoList.Application.Services
+{
+    internal class UserContext(IHttpContextAccessor httpContextAccessor) : IUserContext
+    {
+        public bool IsAuthenticated =>
+            httpContextAccessor
+                .HttpContext?
+                .User
+                .Identity?
+                .IsAuthenticated ??
+            throw new ApplicationException("User context is unavailable");
+
+        public Guid UserId => Guid.Parse(httpContextAccessor
+            .ConnectedUserId() ?? 
+            throw new ApplicationException("User context is unavailable"));
+
+    }
+}
