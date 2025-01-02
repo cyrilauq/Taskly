a37589511704ff4e626ba19fd8db804ddbb7aecb
Cyril Auquier <cyrilauquier@hotmail.fr>
2025-01-02 17:56:56 +0100

    setting up the login form

---
+++ client-web/TodoList.Web/Model/LoginModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoList.Web.Model
+{
+    public class LoginModel
+    {
+        [Required]
+        [MinLength(8)]
+        public string Password { get; set; }
+        [Required]
+        [MinLength(5)]
+        public string Login { get; set; }
+    }
+}
---
+++ client-web/TodoList.Web/Pages/Login.razor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System.Diagnostics;
+using TodoList.Web.Application.Services.Interfaces;
+using TodoList.Web.Model;
+
+namespace TodoList.Web.Pages
+{
+    public partial class Login
+    {
+        [Inject]
+        public IAuthenticationService AuthenticationService { get; set; }
+
+        public LoginModel LoginModel { get; set; } = new LoginModel();
+        public EditContext EditContext { get; set; }
+
+        protected override void OnInitialized()
+        {
+            EditContext = new EditContext(LoginModel);
+
+            base.OnInitialized();
+        }
+
+        public async Task OnSubmit()
+        {
+            if (await AuthenticationService.LoginWithCredentials(LoginModel.Login, LoginModel.Password))
+            {
+                Debug.WriteLine("Successfully logged in");
+            }
+        }
+    }
+}
