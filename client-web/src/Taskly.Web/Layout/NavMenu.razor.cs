0d2fdd4a57daf92d6029252e53fca98ade2944d6
Cyril Auquier <cyrilauquier@hotmail.fr>
2025-01-05 19:45:45 +0100

    display authenticated user's username

---
+++ client-web/src/Taskly.Web/Layout/NavMenu.razor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace Taskly.Web.Layout
+{
+    partial class NavMenu: ComponentBase
+    {
+        [Inject]
+        private AuthenticationStateProvider AuthStateProvider { get; set; }
+
+        public bool UserIsAuthenticated = false;
+        public string UserName { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            AuthStateProvider.AuthenticationStateChanged += OnAuthStateChanged;
+
+            await base.OnInitializedAsync();
+        }
+
+        private void OnAuthStateChanged(Task<AuthenticationState> task)
+        {
+            var currentState = task.Result;
+            UserIsAuthenticated = currentState.User.Identity != null;
+            if (UserIsAuthenticated)
+            {
+                UserName = currentState.User.Identity.Name;
+                StateHasChanged();
+            }
+        }
+    }
+}
