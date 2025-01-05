b6436b70bfff8f39d2e19df857637cfa0d186dc9
Cyril Auquier <cyrilauquier@hotmail.fr>
2025-01-05 11:17:02 +0100

    add http handler for handling http response status code

--- client-web/src/Taskly.Web.Application/ConfigureService.cs
+++ client-web/src/Taskly.Web.Application/ConfigureService.cs
@@ -1,8 +1,6 @@
-﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Taskly.Web.Application.Http;
-using Taskly.Web.Application.Http.Client;
+using Taskly.Web.Application.Http.Handler;
 using Taskly.Web.Application.Services;
 using Taskly.Web.Application.Services.Interfaces;
 using Taskly.Web.Application.State;
@@ -17,7 +15,12 @@ namespace Taskly.Web.Application
             services.AddApplicationServices();
             services.AddApplicationStates();
 
-            services.AddScoped<HttpClient>(factory => new TasklyHttpClient() { BaseAddress = new Uri(configuration["ApiUrl"] ?? throw new Exception("No url given for the api")) });
+            services.AddScoped<HttpStatusHandler>();
+
+            services.AddHttpClient("TasklyHttpClient", cf => cf.BaseAddress = new Uri(configuration["ApiUrl"] ?? throw new Exception("No url given for the api")))
+                .AddHttpMessageHandler<HttpStatusHandler>();
+
+            services.AddTransient(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("TasklyHttpClient"));
 
             return services;
         }
--- client-web/src/Taskly.Web.Application/Http/Handler/HttpStatusHandler.cs
+++ client-web/src/Taskly.Web.Application/Http/Handler/HttpStatusHandler.cs
@@ -1,19 +1,21 @@
 ﻿using System.Net;
 using Taskly.Web.Exceptions;
 
-namespace Taskly.Web.Application.Http.Client
+namespace Taskly.Web.Application.Http.Handler
 {
-    internal class TasklyHttpClient: HttpClient
+    public class HttpStatusHandler: DelegatingHandler
     {
-        public override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
             HttpResponseMessage response = await base.SendAsync(request, cancellationToken);
-            if(response.IsSuccessStatusCode) return response;
+            if (response.IsSuccessStatusCode) return response;
             HttpStatusCode respondeCodeStatus = response.StatusCode;
             switch (respondeCodeStatus)
             {
                 case HttpStatusCode.NotFound:
                     throw new NotFoundException("Resource not found");
+                case HttpStatusCode.InternalServerError:
+                    throw new InternalServerErrorException("An error occured in the server");
                 default:
                     throw new UnExpectedHttpException("");
             }
