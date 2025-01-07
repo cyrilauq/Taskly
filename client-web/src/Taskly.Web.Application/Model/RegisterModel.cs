9fcb4e52c065a072da6285498dd3ce52b9767d74
Cyril Auquier <cyrilauquier@hotmail.fr>
2025-01-07 08:51:32 +0100

    add registering page

---
+++ client-web/src/Taskly.Web.Application/Model/RegisterModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Taskly.Web.Application.Model
+{
+    public class RegisterModel
+    {
+        [Required]
+        [MinLength(3)]
+        public string Firstname { get; set; }
+        [Required]
+        [MinLength(3)]
+        public string Lastname { get; set; }
+        [Required]
+        [StringLength(20, MinimumLength = 3)]
+        public string Pseudo { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [DataType(DataType.Password)]
+        [Required]
+        [MinLength(8)]
+        public string Password { get; set; }
+        [DataType(DataType.Password)]
+        [Required]
+        [Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; }
+        [DataType(DataType.Date)]
+        [Required]
+        public DateOnly BirthDate { get; set; }
+    }
+}
---
+++ client-web/src/Taskly.Web/Pages/Register.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+using Taskly.Web.Application.Model;
+
+namespace Taskly.Web.Pages
+{
+    partial class Register: ComponentBase
+    {
+        RegisterModel FormModel = new();
+
+        public void OnSubmit()
+        {
+
+        }
+    }
+}
