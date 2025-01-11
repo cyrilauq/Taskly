27e49e134cd1f0db2ae5bbd4a55a32b99b4b76cf
Cyril Auquier <cyrilauquier@hotmail.fr>
2025-01-11 13:28:22 +0100

    add a todo model to display todo in view

--- client-web/src/Taskly.Web.Application/Model/TodoModel.cs
+++ client-web/src/Taskly.Web.Application/Model/TodoModel.cs
@@ -1,4 +1,20 @@
-﻿namespace Taskly.Web.Application.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Taskly.Web.Application.Model
 {
-    public record TodoModel(string Id, string Content, string Name, Guid UserId, bool IsDone, DateTime? CreatedOn, DateTime? DeletedOn);
+    public class TodoModel
+    {
+        public string Id { get; set; }
+        [Required]
+        [StringLength(150, MinimumLength = 5)]
+        public string Content { get; set; }
+        [Required]
+        [StringLength(25, MinimumLength = 5)]
+        public string Name { get; set; }
+        public Guid UserId { get; set; }
+        public bool IsDone { get; set; }
+        public DateTime? CreatedOn { get; set; }
+        public DateTime? DeletedOn { get; set; }
+    }
 }
+
