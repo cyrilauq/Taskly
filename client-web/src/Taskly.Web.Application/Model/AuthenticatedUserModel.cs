9e495879beb159cd215c0f8b5695214247b74c24
Cyril Auquier <cyrilauquier@hotmail.fr>
2025-01-11 13:26:52 +0100

    add an authentication model so user and authenticated user aren't the same

---
+++ client-web/src/Taskly.Web.Application/Model/AuthenticatedUserModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Taskly.Web.Application.Model
+{
+    public class AuthenticatedUserModel
+    {
+        public string Pseudo { get; set; }
+        public Guid Id { get; set; }
+        public string Token { get; set; }
+    }
+}
