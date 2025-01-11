152407adcad354d667bd66f25ccfdb35d5ac3e64
Cyril Auquier <cyrilauquier@hotmail.fr>
2025-01-11 13:26:33 +0100

    implement "IAuthState" changes

--- client-web/src/Taskly.Web.Application/State/AuthState.cs
+++ client-web/src/Taskly.Web.Application/State/AuthState.cs
@@ -5,9 +5,10 @@ namespace Taskly.Web.Application.State
 {
     internal class AuthState : IAuthState
     {
-        public bool IsAuthenticated { get => UserName != null; }
+        public bool IsAuthenticated { get => UserName != Token; }
         public string? UserName { get; set; }
         public Guid? UserId { get; set; }
+        public string? Token { get; set; }
 
         public event StateChangedHandler OnStateChange;
 
