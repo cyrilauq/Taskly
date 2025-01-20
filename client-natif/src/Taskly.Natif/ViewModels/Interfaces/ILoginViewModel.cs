4371c760cac9193c44f80747d3dec29e3e3fc9d4
Cyril Auquier <cyrilauquier@hotmail.fr>
2025-01-20 19:20:13 +0100

    add login view model

---
+++ client-natif/src/Taskly.Natif/ViewModels/Interfaces/ILoginViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Taskly.Natif.ViewModels.Interfaces
+{
+    internal interface ILoginViewModel
+    {
+        string Login { get; set; }
+        string Password { get; set; }
+
+        Task<bool> OnLogin();
+    }
+}
---
+++ client-natif/src/Taskly.Natif/ViewModels/LoginViewModel.cs
@@ -0,0 +1,43 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Windows.Input;
+using Taskly.Client.Application.Services.Interfaces;
+
+namespace Taskly.Natif.ViewModels
+{
+    public partial class LoginViewModel : ObservableObject
+    {
+        private IAuthenticationService _authenticationService;
+        private string _login;
+        private string _password;
+
+        public string Login
+        {
+            get => _login;
+            set => SetProperty(ref _login, value);
+        }
+        public string Password
+        {
+            get => _password;
+            set => SetProperty(ref _password, value);
+        }
+        public bool FormCanBeEdited { get; private set; } = true;
+
+        public ICommand OnLoginCommand { private set; get; }
+
+        public LoginViewModel(IAuthenticationService authenticationService)
+        {
+            _authenticationService = authenticationService;
+
+            OnLoginCommand = new AsyncRelayCommand(OnLogin);
+        }
+
+        public async Task<bool> OnLogin()
+        {
+            FormCanBeEdited = false;
+            var result = await _authenticationService.LoginWithCredentials(Login, Password);
+            FormCanBeEdited = true;
+            return result;
+        }
+    }
+}
