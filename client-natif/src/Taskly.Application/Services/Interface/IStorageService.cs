ccdd415316610f7385c1cb26fce5adc51f5ef30e
Cyril Auquier <cyrilauquier@hotmail.fr>
2025-01-22 16:32:14 +0100

    - rename project - create storage manipulation interface

---
+++ client-natif/src/Taskly.Application/Services/Interface/IStorageService.cs
@@ -0,0 +1,8 @@
+﻿namespace Taskly.Natif.Application.Services.Interface
+{
+    public interface IStorageService
+    {
+        Task<bool> SaveAsync<T>(T value, string? key = null);
+        Task<T?> GetAsync<T>(string? key = null);
+    }
+}
