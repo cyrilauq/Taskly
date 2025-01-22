4c9623f031c34515555d54fdc76a9ab11ee16287
Cyril Auquier <cyrilauquier@hotmail.fr>
2025-01-22 16:33:29 +0100

    store object in preferences

---
+++ client-natif/src/Taskly.Natif/Services/NatifStorageService.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using Taskly.Natif.Application.Services.Interface;
+
+namespace Taskly.Natif.Services
+{
+    internal class NatifStorageService : IStorageService
+    {
+        public Task<bool> SaveAsync<T>(T value, string? key = null)
+        {
+            string keyValue = key is null ? nameof(T) : key;
+            Preferences.Default.Set(key, JsonConvert.SerializeObject(value));
+            return Task.FromResult(true);
+        }
+
+        public Task<T?> GetAsync<T>(string? key = null)
+        {
+            string keyValue = key is null ? nameof(T) : key;
+            string stringValue = Preferences.Default.Get<string>(keyValue, "");
+
+            return Task.FromResult(JsonConvert.DeserializeObject<T>(stringValue));
+        }
+    }
+}
