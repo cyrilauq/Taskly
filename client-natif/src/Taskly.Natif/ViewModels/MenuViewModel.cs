b706312996e378aedc27296c17b1177c622af1e0
Cyril Auquier <cyrilauquier@hotmail.fr>
2025-01-24 21:39:38 +0100

    set null for auth state and navigate to home page on logout

--- client-natif/src/Taskly.Natif/ViewModels/MenuViewModel.cs
+++ client-natif/src/Taskly.Natif/ViewModels/MenuViewModel.cs
@@ -1,19 +1,32 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Taskly.Client.Application.State.Interfaces;
+using Taskly.Natif.Application.Services.Interface;
 
 namespace Taskly.Natif.ViewModels
 {
     public partial class MenuViewModel : ObservableObject
     {
+        private IStorageService _storageService;
+
         [ObservableProperty]
         private string? connectedUser;
 
-        public MenuViewModel(IAuthState authState)
+        public MenuViewModel(IAuthState authState, IStorageService storageService)
         {
+            _storageService = storageService;
+
             authState.OnStateChange += () =>
             {
                 ConnectedUser = authState.UserName;
             };
+        }
+
+        [RelayCommand]
+        private async Task OnLogoutAsync()
+        {
+            await _storageService.SaveAsync<IAuthState>(null, nameof(IAuthState));
+            await Shell.Current.GoToAsync("//MainPage");
         }
     }
 }
