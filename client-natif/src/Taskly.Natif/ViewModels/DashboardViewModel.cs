9a3bfbfad5455e68c0347b65c8a5f6ee375f5b4c
Cyril Auquier <cyrilauquier@hotmail.fr>
2025-01-24 16:24:45 +0100

    display toast on error

--- client-natif/src/Taskly.Natif/ViewModels/DashboardViewModel.cs
+++ client-natif/src/Taskly.Natif/ViewModels/DashboardViewModel.cs
@@ -1,7 +1,10 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Taskly.Client.Application.Exceptions;
 using Taskly.Client.Application.Model;
 using Taskly.Client.Application.Services.Interfaces;
+using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Maui.Core;
 
 namespace Taskly.Natif.ViewModels
 {
@@ -13,7 +16,22 @@ namespace Taskly.Natif.ViewModels
         [RelayCommand]
         private async Task OnPageLoadedAsync()
         {
-            Todos = new List<TodoModel>(await todoService.GetConnectedUserTodos());
+            try
+            {
+                // TODO : Display delete button when todo is swiped from left to right
+                // TODO : Display edit button when todo is swiped from rigth to left
+                Todos = new List<TodoModel>(await todoService.GetConnectedUserTodos());
+            }
+            catch (ServiceException se)
+            {
+                var toast = Toast.Make(se.Message, ToastDuration.Long, 14);
+                await toast.Show();
+            }
+            catch (Exception se)
+            {
+                var toast = Toast.Make("Unexpected error", ToastDuration.Long, 14);
+                await toast.Show();
+            }
         }
     }
 }
