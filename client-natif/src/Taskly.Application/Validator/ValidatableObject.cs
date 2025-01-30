238985c585c1425a52221fc808c30721c5d6695a
Cyril Auquier <cyrilauquier@hotmail.fr>
2025-01-30 20:01:31 +0100

    add observable validator so they can be used in component

---
+++ client-natif/src/Taskly.Application/Validator/ValidatableObject.cs
@@ -0,0 +1,37 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Taskly.Natif.Application.Validator.Rules;
+
+namespace Taskly.Natif.Application.Validator
+{
+    public class ValidatableObject<T> : ObservableObject, IValidatorObject
+    {
+        private bool _hasError;
+        private object _value;
+        private string _error;
+        public bool HasError
+        {
+            get => _hasError;
+            private set => SetProperty(ref _hasError, value);
+        }
+
+        public List<IValidationRule<T>> Rules { get; set; } = new();
+        public string? Error
+        {
+            get => _error;
+            private set => SetProperty(ref _error, value);
+        }
+        public object? Value
+        {
+            get => _value;
+            set => SetProperty(ref _value, value);
+        }
+
+        public bool Validate()
+        {
+            var errors = Rules.Where(r => r.Check((T)Value)).ToList();
+            Error = errors.Any() ? errors[0].ValidationMessage : null;
+            HasError = errors.Any();
+            return errors.Any();
+        }
+    }
+}
