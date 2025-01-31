ca3eb3de831eb9908cf6b4e1ca9c6229dfd8a760
Cyril Auquier <cyrilauquier@hotmail.fr>
2025-01-31 12:27:47 +0100

    add a required rule for string type

---
+++ client-natif/src/Taskly.Application/Validator/Rules/StringRequiredRule.cs
@@ -0,0 +1,12 @@
+﻿namespace Taskly.Natif.Application.Validator.Rules
+{
+    public class StringRequiredRule : IValidationRule<string>
+    {
+        public string ValidationMessage { get; init; }
+
+        public bool Check(string value)
+        {
+            return string.IsNullOrEmpty(value);
+        }
+    }
+}
