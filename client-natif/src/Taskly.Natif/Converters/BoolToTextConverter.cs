d3f8823f7c8fa62b9501460a3fdce21b9491ee17
Cyril Auquier <cyrilauquier@hotmail.fr>
2025-02-09 15:31:20 +0100

    introduce converters to transfer bool into text

---
+++ client-natif/src/Taskly.Natif/Converters/BoolToTextConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace Taskly.Natif.Converters
+{
+    internal class BoolToTextConverter : IValueConverter
+    {
+        public required string TrueText { get; set; }
+        public required string FalseText { get; set; }
+
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            bool isDone = value is null ? false : (bool)value;
+            return isDone ? TrueText : FalseText;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
