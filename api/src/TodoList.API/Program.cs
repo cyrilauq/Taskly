33dda69cf13d40a2a577381cb0ec8a38a55db47b
Cyril Auquier <cyrilauquier@hotmail.fr>
2025-02-21 17:00:47 +0100

    config api to run on local iis

--- api/src/TodoList.API/Program.cs
+++ api/src/TodoList.API/Program.cs
@@ -30,11 +30,8 @@ builder.Services.AddSwagger();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseMiddleware<ExceptionsMiddleware>();
 
