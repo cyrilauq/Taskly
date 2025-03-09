9b8c581e2e4fdd5dd15895c33cd47fc424a0e2e8
Cyril Auquier <cyrilauquier@hotmail.fr>
2025-03-09 20:22:30 +0100

    create a service to manage application navigation

---
+++ client-natif/src/Taskly.Application/Services/INavigationService.cs
@@ -0,0 +1,7 @@
+﻿namespace Taskly.Natif.Application.Services
+{
+    public interface INavigationService
+    {
+        Task<bool> NavigateTo(string url);
+    }
+}
---
+++ client-natif/src/Taskly.Natif/Services/NatifNavigationService.cs
@@ -0,0 +1,12 @@
+﻿using Taskly.Natif.Application.Services;
+
+namespace Taskly.Natif.Services
+{
+    public class NatifNavigationService : INavigationService
+    {
+        public Task<bool> NavigateTo(string url)
+        {
+            return Task.FromResult(Microsoft.Maui.Controls.Application.Current?.Dispatcher.Dispatch(async () => await Shell.Current.GoToAsync(url, false)) ?? false);
+        }
+    }
+}
