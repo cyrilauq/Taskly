80515a376e01301bd39197ec5f9b79416a88f0c3
Cyril Auquier <cyrilauquier@hotmail.fr>
2025-03-15 20:37:10 +0100

    add rule for required date

---
+++ client-natif/src/Taskly.Application/Validator/Rules/DateRequiredRule.cs
@@ -0,0 +1,12 @@
+﻿namespace Taskly.Natif.Application.Validator.Rules
+{
+    public class DateRequiredRule : IValidationRule<DateOnly>
+    {
+        public string ValidationMessage { get; init; }
+
+        public bool Check(DateOnly value)
+        {
+            return value != DateOnly.MinValue && value != DateOnly.FromDateTime(DateTime.Now);
+        }
+    }
+}
