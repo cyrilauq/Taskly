e3779dccefa0facc6449d8c236aa6582a43f8ec0
Cyril Auquier <cyrilauquier@hotmail.fr>
2025-03-23 20:22:17 +0100

    add viewmodel test project

---
+++ client-natif/tests/Taskly.Natif.ViewModels.Test/RegisterViewModelTests.cs
@@ -0,0 +1,32 @@
+﻿namespace Taskly.Natif.ViewModels.Test
+{
+    [TestClass]
+    public sealed class RegisterViewModelTests
+    {
+        [TestInitialize]
+        public void SetUp()
+        {
+            // Arrange
+        }
+
+        [TestMethod]
+        public void When_RegisterIsCalledAndValidatorHasValidData_Then_ServiceIsCalledWithRigthArgs()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [TestMethod]
+        public void When_RegisterIsCalledAndValidatorHasNoValidData_Then_ServiceIsNotCalled()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
